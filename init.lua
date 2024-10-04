a5c21df84a6ea7ab95c1be87de42df8701fc225a
julianbopp <julian.bopp@tutanota.com>
2024-10-04 13:08:40 +0200

    Use Black as python formatter

--- init.lua
+++ init.lua
@@ -678,7 +678,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         xml = { 'xmlformat' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'black' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
