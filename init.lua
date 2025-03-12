1608b46954f720e7d9115a70ac04acfdbb51a0cd
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-03-12 17:24:44 -0400

    feat: add `vim.opt.confirm = true` (#1384)

--- init.lua
+++ init.lua
@@ -175,6 +175,10 @@ vim.opt.expandtab = true
 -- set vim-slime target
 vim.g.slime_target = 'tmux'
 vim.g.slime_python_ipython = 1
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
