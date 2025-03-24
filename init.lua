0f63b2dde4e234a66087fa90556975f9daa94006
RulentWave <49258216+RulentWave@users.noreply.github.com>
2025-03-24 15:33:53 -0400

    changed Conform's format_on_save lambda so that buffers that match disable_filetypes return nil. This allows you to enable a formatter for langages in the disable_filetypes table to have a formatter that can be run manually with Leader-f but doesnt enable format_on_save for them (#1395)

--- init.lua
+++ init.lua
@@ -847,14 +847,13 @@ require('lazy').setup({
         local disable_filetypes = { c = true, cpp = true, xml = false, conaryrecipe = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
+          return nil
         else
-          lsp_format_opt = 'fallback'
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
         end
-        return {
-          timeout_ms = 5000,
-          lsp_format = lsp_format_opt,
-        }
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
